13aaf52ceeec483e45f385a9a40ef72f5418c0ee
jordantwombly <jordantwombly@gmail.com>
2016-12-07 21:09:23 -0500

    Implemented Login and Ebook Forms

--- Facade_Adapter/Facade_Adapter/Forms/EbookForm.Designer.cs
+++ Facade_Adapter/Facade_Adapter/Forms/EbookForm.Designer.cs
@@ -28,11 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Book_Listbox = new System.Windows.Forms.ListBox();
+            this.Accept_Button = new System.Windows.Forms.Button();
+            this.Cancel_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Book_Listbox
+            // 
+            this.Book_Listbox.FormattingEnabled = true;
+            this.Book_Listbox.Location = new System.Drawing.Point(13, 13);
+            this.Book_Listbox.Name = "Book_Listbox";
+            this.Book_Listbox.Size = new System.Drawing.Size(557, 199);
+            this.Book_Listbox.TabIndex = 0;
+            // 
+            // Accept_Button
+            // 
+            this.Accept_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Accept_Button.Location = new System.Drawing.Point(142, 218);
+            this.Accept_Button.Name = "Accept_Button";
+            this.Accept_Button.Size = new System.Drawing.Size(145, 51);
+            this.Accept_Button.TabIndex = 1;
+            this.Accept_Button.Text = "Accept";
+            this.Accept_Button.UseVisualStyleBackColor = true;
+            this.Accept_Button.Click += new System.EventHandler(this.Accept_Button_Click);
+            // 
+            // Cancel_Button
+            // 
+            this.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancel_Button.Location = new System.Drawing.Point(318, 218);
+            this.Cancel_Button.Name = "Cancel_Button";
+            this.Cancel_Button.Size = new System.Drawing.Size(145, 51);
+            this.Cancel_Button.TabIndex = 2;
+            this.Cancel_Button.Text = "Cancel";
+            this.Cancel_Button.UseVisualStyleBackColor = true;
+            // 
+            // EbookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "EbookForm";
+            this.CancelButton = this.Cancel_Button;
+            this.ClientSize = new System.Drawing.Size(582, 281);
+            this.Controls.Add(this.Cancel_Button);
+            this.Controls.Add(this.Accept_Button);
+            this.Controls.Add(this.Book_Listbox);
+            this.Name = "EbookForm";
+            this.Text = "Ebook Collection";
+            this.Load += new System.EventHandler(this.EbookForm_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox Book_Listbox;
+        private System.Windows.Forms.Button Accept_Button;
+        private System.Windows.Forms.Button Cancel_Button;
     }
 }
